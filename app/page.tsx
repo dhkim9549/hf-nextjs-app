ff630331d75eb37159ba47e2ca273d998ba539b7
root <dhkim95@hotmail.com>
2024-01-09 18:01:39 +0900

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import Footer from './footer.tsx'
+import Title from '@/app/ui/title'
 
 function Header() {
   return (
@@ -10,25 +11,6 @@ function Header() {
         height={41}
         alt="한국주택금융공사"
       />
-    </div>
-  )
-}
-
-function Title() {
-  return (
-    <div className="text-center lg:text-left lg:ml-10 my-10 px-10">
-      <blockquote className="text-2xl font-bold italic text-slate-900">
-        HF
-        <span className="ml-3 mr-2 before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-pink-500 relative inline-block">
-          <span className="relative text-white">Open</span>
-        </span>
-        <span>
-          API
-        </span>
-        <span className="ml-2">
-          온라인전시관
-        </span>
-    	</blockquote>
     </div>
   )
 }
@@ -46,17 +28,17 @@ export default function Home() {
   return (
     <>
       <Header/>
-      <Title/>
+      <Title t1="HF" t2="Open" t3="온라인 전시관"/>
       <div className="w-full p-4 flex flex-col lg:flex-row lg:flex-wrap">
         <Item
           title="HF 전세자금보증 추천"
           cont="한국주택금융공사에서 제공하는 전세보증 상품 추천 서비스"
-           href="hf-api/hg-jnse-rcmd.html"
+          href="hf-api/hg-jnse-rcmd.html"
         />
         <Item
           title="은행별 전세대출 금리"
           cont="한국주택금융공사 보증서 담보 시중은행 대출의 고객 특성별 금리"
-           href="hf-api/rent-loan-multi-info.html"
+          href="hf-api/rent-loan-multi-info"
         />
       </div>
       <Footer/>
---
+++ app/ui/title.tsx
@@ -0,0 +1,15 @@
+export default function Title({t1, t2, t3}) {
+  return (
+    <div className="text-center lg:text-left lg:ml-10 my-10 px-10">
+      <blockquote className="text-2xl font-bold italic text-slate-900">
+        {t1}
+        <span className="ml-3 mr-2 before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-pink-500 relative inline-block">
+          <span className="relative text-white">{t2}</span>
+        </span>
+        <span className="ml-2">
+          {t3}
+        </span>
+      </blockquote>
+    </div>
+  )
+}

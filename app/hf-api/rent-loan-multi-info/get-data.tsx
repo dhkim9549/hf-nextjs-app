b7bed7f650760c4d780810b36d33c43ec98bf7d0
root <dhkim95@hotmail.com>
2024-01-23 20:01:29 +0900

    first commit

---
+++ app/hf-api/rent-loan-multi-info/get-data.tsx
@@ -0,0 +1,21 @@
+'use server'
+
+export async function getLoanRateData(loanYm) {
+
+  let apiStr = ""
+    + "?serviceKey=PW2VvwTvkcs%2FWMVLduXzeRL0BPjOYH%2B0wMnsQiyy5UgcrukEjAurATJUNkeA7T%2Bj47s3GAmLzHduip%2BfbxESlQ%3D%3D"
+    + "&pageNo=1"
+    + "&numOfRows=30"
+    + "&dataType=JSON"
+    ;
+
+  let res = await fetch("https://apis.data.go.kr/B551408/rent-loan-rate-multi-dimensional-info/dimensional-list"
+    + apiStr
+    + "&loanYm=" + loanYm,
+    { next: { revalidate: 3600 } }
+  );
+
+  let rentJson = await res.json();
+
+  return rentJson.body.items;
+}

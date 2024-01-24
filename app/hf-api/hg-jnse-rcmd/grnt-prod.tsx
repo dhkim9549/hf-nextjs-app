4e9b6b6edc6266072818a6d90bd7ddebc1fd2f7b
root <dhkim95@hotmail.com>
2024-01-24 19:26:14 +0900

    add content

--- app/hf-api/hg-jnse-rcmd/grnt-prod.tsx
+++ app/hf-api/hg-jnse-rcmd/grnt-prod.tsx
@@ -20,13 +20,24 @@ export default function GrntProd({prodObj}) {
     getProdInfoWrap();
   }, []);
 
+  const listItems = prodInfo?.reqTrgtCont.split('|').map(x =>
+    <li>
+      {x}
+    </li>
+  );
+
   return (
-    <Paper className="w-full lg:w-[300px] bg-white p-4 flex flex-col gap-2">
+    <Paper className="w-full lg:w-[350px] bg-white p-4 flex flex-col gap-2">
       <div className="text-slate-800 text-2xl mt-5 mb-2">{prodInfo ? prodInfo.rcmdProdNm : "loading..."}</div>
       <div className="text-slate-900">추천순위: {prodObj.rcmdProrRnk}</div>
       <div className="text-slate-900">보증구분코드: {prodObj.grntDvcd}</div>
       <div className="text-green-900">보증한도: <NumericFormat value={prodObj.grntLmtAmt} thousandSeparator="," /></div>
       <div className="text-green-900">대출한도: <NumericFormat value={prodObj.loanLmtAmt} thousandSeparator="," /></div>
+      <div className="text-slate-900">추천보증상품구분: {prodInfo?.rcmdGrntProdDvcd}</div>
+      <div className="text-slate-900">신청대상 요약:</div>
+        <ul className="list-disc text-sm px-6">{listItems}</ul>
+      <div className="text-slate-900">문의처: {prodInfo?.qscNm}</div>
+      <div className="text-slate-900">문의처 전화번호: {prodInfo?.qscTlno}</div>
     </Paper>
   )
 }

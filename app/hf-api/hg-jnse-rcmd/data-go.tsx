ed324822e03f2db27f3e1e719513443702b1a300
root <dhkim95@hotmail.com>
2024-01-25 18:48:23 +0900

    first commit

---
+++ app/hf-api/hg-jnse-rcmd/data-go.tsx
@@ -0,0 +1,18 @@
+export default function DataGo() {
+  return (
+    <div id="div2" className="m-4 mt-20">
+      <dl className="p-4" style={{border:"1px solid #e0e0e0", borderTop:"3px solid #655bc2"}}>
+        <dt>
+          <a href="https://www.data.go.kr/data/15119396/openapi.do" target="_blank">
+            <span className="text-xl text-slate-800 font-medium">
+              한국주택금융공사_전세자금보증상품 추천서비스	
+            </span>
+          </a>
+        </dt>
+        <dd className="mt-2">
+          한국주택금융공사의 전세자금보증 데이터로, 고객특성별 전세자금보증상품 추천, 전세자금보증상품 월별 평균금리 조회, 전세자금보증상품 상세정보 조회, 전세자금보증상품 지역별 최대임차보증금액 조회 서비스를 제공합니다.
+        </dd>
+      </dl>
+    </div>
+  );
+}

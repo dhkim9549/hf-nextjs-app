53d0eb2c2cdaa94ff2f18a523129560b73a85710
root <dhkim95@hotmail.com>
2024-01-26 21:17:05 +0900

    add content

--- app/hf-api/hg-jnse-rcmd/grnt-prod.tsx
+++ app/hf-api/hg-jnse-rcmd/grnt-prod.tsx
@@ -21,7 +21,7 @@ export default function GrntProd({prodObj}) {
   }, []);
 
   const listItems = prodInfo?.reqTrgtCont.split('|').map(x =>
-    <li>
+    <li key={x}>
       {x}
     </li>
   );
@@ -31,13 +31,19 @@ export default function GrntProd({prodObj}) {
       <div className="text-slate-800 text-2xl mt-5 mb-2">{prodInfo ? prodInfo.rcmdProdNm : "loading..."}</div>
       <div className="text-slate-900">추천순위: {prodObj.rcmdProrRnk}</div>
       <div className="text-slate-900">보증구분코드: {prodObj.grntDvcd}</div>
-      <div className="text-green-900">보증한도: <NumericFormat value={prodObj.grntLmtAmt} thousandSeparator="," /></div>
-      <div className="text-green-900">대출한도: <NumericFormat value={prodObj.loanLmtAmt} thousandSeparator="," /></div>
+      <div className="text-slate-900">보증한도: {prodObj.grntLmtAmt.replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</div>
+      <div className="text-slate-900">대출한도: {prodObj.loanLmtAmt.replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</div>
       <div className="text-slate-900">추천보증상품구분: {prodInfo?.rcmdGrntProdDvcd}</div>
       <div className="text-slate-900">신청대상 요약:</div>
         <ul className="list-disc text-sm px-6">{listItems}</ul>
       <div className="text-slate-900">문의처: {prodInfo?.qscNm}</div>
       <div className="text-slate-900">문의처 전화번호: {prodInfo?.qscTlno}</div>
+      <div className="text-slate-900">안내링크:
+        <a className="block text-sm ml-4 underline text-blue-600 hover:text-blue-800 visited:text-purple-600" href={prodInfo?.guidUrl} target="_blank">{prodInfo?.guidUrl}</a>
+      </div>
+      <div className="text-slate-900">이자지원내용: {prodInfo?.intSprtCont}</div>
+      <div className="text-slate-900">예상 보증료율: {prodInfo?.exptGrfeRateCont}</div>
+      <div className="text-slate-900">우대내용: {prodInfo?.grntPrmeCont}</div>
     </Paper>
   )
 }

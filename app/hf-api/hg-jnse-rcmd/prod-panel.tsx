88e2b1abd119ef6b29c069f454ad733e2776ed09
root <dhkim95@hotmail.com>
2024-01-29 15:48:22 +0900

    first commit

---
+++ app/hf-api/hg-jnse-rcmd/prod-panel.tsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react';
+import { getRcmdProdData } from './get-data';
+import GrntProdItem from './grnt-prod-item';
+
+export default function ProdPanel({queryObj}) {
+
+  let [rcmdItems, setRcmdItems] = useState();
+  let [prodInfoMap, setProdInfoMap] = useState();
+  let [maxRentAmtMap, setMaxRentAmtMap] = useState();
+
+  useEffect(() => {
+    getRcmdProdData(queryObj).then((x) => {
+      setRcmdItems(x.rcmdItems);
+      setProdInfoMap(x.prodInfoMap);
+      setMaxRentAmtMap(x.maxRentAmtMap);
+    });
+  }, [queryObj]);
+
+  return (
+    <div>
+      <div className="m-4 flex flex-wrap gap-3">
+       {rcmdItems ? rcmdItems.map(x => 
+         <GrntProdItem
+           key={x.rcmdProrRnk}
+           prodObj={x}
+           prodInfo={prodInfoMap.get(x.grntDvcd)}
+           maxRentAmtList={maxRentAmtMap.get(x.grntDvcd)}
+         />
+       ) : "loading..."}
+      </div>
+      {rcmdItems &&
+        <div className="m-4 p-4">
+          {rcmdItems.length} 건이 조회되었습니다.
+        </div>
+      }
+    </div>
+  )
+}

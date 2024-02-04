bc61f494ba4bfd751c1dec139067494f36900895
root <dhkim95@hotmail.com>
2024-02-04 14:00:45 +0900

    add valueFormatter

--- app/hf-api/rent-loan-multi-info/m-table.tsx
+++ app/hf-api/rent-loan-multi-info/m-table.tsx
@@ -3,7 +3,14 @@ import { NumericFormat } from 'react-number-format';
 
 const columns = [
   { field: 'bankNm', headerName: '은행', width: 100, headerAlign: 'center', align: 'center' },
-  { field: 'avgLoanRat2', headerName: '대출금리(%)', width: 95, type: 'number', headerAlign: 'center' },
+  { field: 'avgLoanRat2', headerName: '대출금리(%)', width: 95, type: 'number', headerAlign: 'center',
+    valueFormatter: (params) => {
+      if (params.value == null) {
+        return '';
+      }
+      return params.value.toLocaleString(undefined, { minimumFractionDigits: 2 });
+    },
+  },
   { field: 'cnt', headerName: '대출실행건수', width: 100, type: 'number', headerAlign: 'center' },
   { field: 'loanAmt', headerName: '대출실행금액(원)', width: 150, type: 'number', headerAlign: 'center'}
 ];
@@ -15,7 +22,7 @@ export default function DataTable({bankList}) {
     let row = {
       "id": e.bankNm,
       "bankNm": e.bankNm,
-      "avgLoanRat2": e.avgLoanRat2,
+      "avgLoanRat2": Number(e.avgLoanRat2),
       "cnt": e.cnt,
       "loanAmt": Number(e.loanAmt)
     };

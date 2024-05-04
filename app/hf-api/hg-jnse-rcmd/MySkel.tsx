15bcdf7f9ff454c67ecd4fb361b67819a74338a2
root <dhkim95@hotmail.com>
2024-05-04 11:13:02 +0900

    fix class

--- app/hf-api/hg-jnse-rcmd/MySkel.tsx
+++ app/hf-api/hg-jnse-rcmd/MySkel.tsx
@@ -1,10 +1,10 @@
 export default function MySkel() {
   return (
-    <div class="w-full lg:w-96 bg-white p-4">
-      <div class="animate-pulse flex flex-col gap-3">
-        <div class="h-6 mt-5 mb-2 bg-slate-200 rounded"></div>
+    <div className="w-full lg:w-96 bg-white p-4">
+      <div className="animate-pulse flex flex-col gap-3">
+        <div className="h-6 mt-5 mb-2 bg-slate-200 rounded"></div>
         {new Array(4).fill('').map(x =>
-          <div class="h-3 bg-slate-200 rounded"></div>
+          <div className="h-3 bg-slate-200 rounded"></div>
         )}
       </div>
     </div>

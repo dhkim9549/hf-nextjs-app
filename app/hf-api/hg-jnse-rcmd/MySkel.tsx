ae7dbae60fa996a0bfe6abe402753a177dd8be2b
root <dhkim95@hotmail.com>
2024-06-21 14:30:29 +0900

    modify MySkel

--- app/hf-api/hg-jnse-rcmd/MySkel.tsx
+++ app/hf-api/hg-jnse-rcmd/MySkel.tsx
@@ -1,11 +1,8 @@
 export default function MySkel() {
   return (
     <div className="w-full lg:w-96 bg-white p-4">
-      <div className="animate-pulse flex flex-col gap-3">
-        <div className="h-6 mt-5 mb-2 bg-slate-200 rounded"></div>
-        {new Array(4).fill('').map(x =>
-          <div className="h-3 bg-slate-200 rounded"></div>
-        )}
+      <div className="animate-pulse flex flex-col">
+        <div className="h-48 mt-2 mb-2 bg-slate-200 rounded"></div>
       </div>
     </div>
   )
--- app/hf-api/rent-loan-multi-info/MySkel.tsx
+++ app/hf-api/rent-loan-multi-info/MySkel.tsx
@@ -2,9 +2,7 @@ export default function MySkel() {
   return (
     <div className="w-full sm:w-[450px]">
       <div className="animate-pulse flex flex-col gap-2">
-        {new Array(4).fill('').map(x =>
-          <div className="h-7 bg-slate-200 rounded"></div>
-        )}
+        <div className="h-60 mt-2 mb-2 bg-slate-200 rounded"></div>
       </div>
     </div>
   )

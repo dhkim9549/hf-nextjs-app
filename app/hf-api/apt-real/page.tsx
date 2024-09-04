5b4466a8248a93da2606f52661e12f0667f656db
dhkim9549 <dhkim95@hotmail.com>
2024-09-04 09:57:20 +0000

    add context

--- app/hf-api/apt-real/page.tsx
+++ app/hf-api/apt-real/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState } from 'react';
+import { useState, createContext, useEffect } from 'react';
 
 import Paper from '@mui/material/Paper';
 import Button from '@mui/material/Button';
@@ -8,36 +8,53 @@ import TextField from '@mui/material/TextField';
 import { NumericFormat } from 'react-number-format';
 
 import ProdPanel from './prod-panel';
+import { AptContext } from './apt-context';
 
 export default function ExpressTest() {
 
   let [aptNm, setAptNm] = useState();
   let [queryObj, setQueryObj] = useState();
+  let [aptList, setAptList] = useState([]);
 
   function setQuery() {
     setQueryObj({aptNm});
   }
 
+  function addAptList(x) {
+    console.log('addAptList() start...');
+    aptList.push(x);
+    setAptList(aptList);
+  }
+
   return (
-    <div>
-      <div className="text-center my-10 py-10 lg:text-left lg:m-10 lg:p-10">
-        <blockquote className="text-2xl font-bold italic text-slate-900">
-          Apt real
-        </blockquote>
+    <AptContext.Provider value={addAptList}>
+      <div>
+        <div className="text-center my-10 py-10 lg:text-left lg:m-10 lg:p-10">
+          <blockquote className="text-2xl font-bold italic text-slate-900">
+            Apt real
+          </blockquote>
+	  <div className="bg-amber-200">
+	    {JSON.stringify(aptList)}
+	  </div>
+        </div>
+        <Paper className="m-4 p-12 flex flex-wrap flex-row lg:flex-row gap-3">
+          <TextField
+            id="aptNm" label="aptNm" variant="filled"
+            className="grow"
+            inputProps={{min: 0, maxLength:20 }}
+            onChange={(e) => {
+              setAptNm(e.target.value);
+            }}
+          />
+          <div className="flex flex-col justify-center">
+            <Button variant="contained" size="large" onClick={setQuery}>조회</Button>
+          </div>
+        </Paper>
+       {queryObj && <ProdPanel queryObj={queryObj} />}
       </div>
-      <Paper className="m-4 p-12 flex flex-wrap flex-col lg:flex-row gap-3 lg:gap-12">
-        <TextField
-          id="aptNm" label="aptNm" variant="filled"
-          inputProps={{min: 0, maxLength:20 }}
-          onChange={(e) => {
-            setAptNm(e.target.value);
-          }}
-        />
-      </Paper>
-      <div className="m-8 lg:mx-20">
-        <Button variant="contained" size="large" onClick={setQuery}>조회</Button>
+      <div className="bg-amber-100">
+        dddd
       </div>
-      {queryObj && <ProdPanel queryObj={queryObj} />}
-    </div>
+    </AptContext.Provider>
   )
 }

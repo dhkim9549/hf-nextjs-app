0370092248d3f4a6b2f1224f58f92320a60e0088
dhkim9549 <dhkim95@hotmail.com>
2024-09-05 07:16:26 +0000

    add sidebar

--- app/hf-api/apt-real/page.tsx
+++ app/hf-api/apt-real/page.tsx
@@ -21,40 +21,44 @@ export default function ExpressTest() {
   }
 
   function addAptList(x) {
-    console.log('addAptList() start...');
     let list = aptList.slice();
     list.push(x);
     setAptList(list);
   }
 
   return (
-    <div>
-      <div>
-        <div className="text-center my-10 py-10 lg:text-left lg:m-10 lg:p-10">
-          <blockquote className="text-2xl font-bold italic text-slate-900">
-            Apt real
-          </blockquote>
-	  <div className="bg-amber-200">
-	    {JSON.stringify(aptList)}
-	  </div>
-        </div>
-        <Paper className="m-4 p-12 flex flex-wrap flex-row lg:flex-row gap-3">
-          <TextField
-            id="aptNm" label="aptNm" variant="filled"
-            className="grow"
-            inputProps={{min: 0, maxLength:20 }}
-            onChange={(e) => {
-              setAptNm(e.target.value);
-            }}
-          />
-          <div className="flex flex-col justify-center">
-            <Button variant="contained" size="large" onClick={setQuery}>조회</Button>
+    <div className="flex flex-row">
+      <div className="w-96">
+        <div className="flex flex-col h-screen">
+          <div className="text-center my-10 py-10 lg:text-left lg:m-4 lg:p-4">
+            <blockquote className="text-2xl font-bold italic text-slate-900">
+              Apt real
+            </blockquote>
           </div>
-        </Paper>
-       {queryObj && <ProdPanel queryObj={queryObj} addAptList={addAptList} />}
+          <Paper className="m-4 p-4 flex flex-wrap flex-row lg:flex-row gap-3">
+            <TextField
+              id="aptNm" label="aptNm" variant="filled"
+              className="grow"
+              inputProps={{min: 0, maxLength:20 }}
+              onChange={(e) => {
+                setAptNm(e.target.value);
+              }}
+	      onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                  setQuery();
+	      }}}
+            />
+            <div className="flex flex-col justify-center">
+              <Button variant="contained" size="large" onClick={setQuery}>조회</Button>
+            </div>
+          </Paper>
+	  <div className="h-screen overflow-y-scroll">
+           {queryObj && <ProdPanel queryObj={queryObj} addAptList={addAptList} />}
+          </div>
+	</div>
       </div>
-      <div className="bg-amber-100">
-        dddd
+      <div className="grow bg-amber-100">
+        {JSON.stringify(aptList)}
       </div>
     </div>
   )

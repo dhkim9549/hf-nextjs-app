19767e38f4146244d0043a2b8a6064a4176b0628
dhkim9549 <dhkim95@hotmail.com>
2024-09-08 06:51:51 +0000

    remove billboard.js react wrapper

--- app/hf-api/rent-loan-multi-info/b-chart.tsx
+++ app/hf-api/rent-loan-multi-info/b-chart.tsx
@@ -3,42 +3,38 @@
 import React, {useEffect, useRef} from "react";
 
 // import billboard.js
-import bb, {pie} from "billboard.js";
+import bb, {treemap} from "billboard.js";
 import "billboard.js/dist/billboard.css";  // default css
-
-// import react wrapper
-import BillboardJS, {IChart} from "@billboard.js/react";
 
 export default function BChart({chartData}) {
 
-  // to get the instance, create ref and pass it to the component
-  const chartComponent = useRef<IChart>();
-  const options = {
-    legend: {
-      position: "right"
-    },
-    data: {
-      columns: [
-      ],
-      type: pie() // for ESM specify as: bar()
-    }
-  };
-
   useEffect(() => {
-    // get the instance from ref
-    const chart = chartComponent.current?.instance;
-
-    // call APIs
-    if (chart) {
-      let chartDataArr = chartData?.chartDataArr;
-
-      chart.load({
-        columns: chartDataArr
-      });
-    }
+    var chart = bb.generate({
+      padding: {
+        top: 10,
+        bottom: 15
+      },
+      data: {
+        columns: chartData.chartDataArr,
+        type: treemap(),
+        labels: {
+          colors: "#fff"
+        }
+      },
+      treemap: {
+        label: {
+          threshold: 0.01,
+          fontSize: 40
+        }
+      },
+      size: {
+        height: 400
+      },
+      bindto: "#chart"
+    });
   }, [chartData]);
 
   return (
-    <BillboardJS bb={bb} options={options} ref={chartComponent} />
+    <div id="chart"></div>
   );
 }

7a49b1999e8cd4c4130a0928cdcf20a981a51813
dhkim9549 <dhkim95@hotmail.com>
2024-09-09 14:19:38 +0000

    change width

--- app/hf-api/apt-real/skel.tsx
+++ app/hf-api/apt-real/skel.tsx
@@ -1,6 +1,6 @@
 export default function Skel() {
   return (
-    <div className="w-full lg:w-96 bg-white p-4">
+    <div className="w-80 bg-white p-4">
       <div className="animate-pulse flex flex-col">
         <div className="h-48 mt-2 mb-2 bg-slate-200 rounded"></div>
       </div>

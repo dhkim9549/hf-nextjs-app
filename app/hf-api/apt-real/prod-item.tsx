7f71ff0b7e1e33670901f358fb0e6a4893262692
dhkim9549 <dhkim95@hotmail.com>
2024-09-13 05:49:42 +0000

    change some UI

--- app/hf-api/apt-real/prod-item.tsx
+++ app/hf-api/apt-real/prod-item.tsx
@@ -5,9 +5,9 @@ export default function ProdItem({prodObj, addAptList}) {
 
   return(
     <Paper className="w-full lg:w-72 bg-white p-4 flex flex-col">
-      <div className="text-slate-800 text-xl mb-1">{prodObj.aptNm}</div>
-      <div className="text-slate-900">{prodObj.sggu}</div>
-      <div className="text-slate-900 text-sm">{prodObj.cnstYr}년 준공</div>
+      <div className="text-black text-xl mb-1">{prodObj.aptNm}</div>
+      <div className="text-slate-800">{prodObj.sggu}</div>
+      <div className="text-slate-800 text-sm">{prodObj.cnstYr}년 준공</div>
       <div className="mt-2 flex flex-wrap gap-2">
         {prodObj.areas &&
           prodObj.areas.map(x =>
@@ -22,7 +22,7 @@ export default function ProdItem({prodObj, addAptList}) {
 function AreaItem({area, sggu, aptNm, addAptList}) {
 
   return(
-    <Button variant="contained" className="min-w-0 h-5 px-1 py-3 bg-amber-300 text-black text-sm" onClick={() => {
+    <Button variant="contained" className="min-w-10 h-8 px-1 py-3 bg-amber-300 text-slate-600 text-sm font-bold" onClick={() => {
       addAptList({sggu, aptNm, area});
     }}>
       {area}

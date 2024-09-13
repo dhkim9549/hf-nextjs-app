6f1f83c9a0510ea25fd330d9d9abaabbd240ad2f
dhkim9549 <dhkim95@hotmail.com>
2024-09-13 02:19:14 +0000

    make responsive

--- app/hf-api/apt-real/prod-panel.tsx
+++ app/hf-api/apt-real/prod-panel.tsx
@@ -22,9 +22,9 @@ export default function ProdPanel({queryObj, addAptList}) {
           {rtnItems.length} 건이 조회되었습니다.
         </div>
       }
-      <div className="flex flex-col gap-3">
+      <div className="flex flex-wrap justify-center lg:flex-col gap-3">
         {rtnItems ?
-          rtnItems.map(x =>
+          rtnItems.slice(0, 10).map(x =>
             <ProdItem key={Math.random()} prodObj={x} addAptList={addAptList}  />
           )
 	  : <Skel />

a0075007dd45cc9ef666d5cb81afb8d6f8b652bc
dhkim9549 <dhkim95@hotmail.com>
2024-09-13 12:22:27 +0000

    change ui

--- app/hf-api/apt-real/skel.tsx
+++ app/hf-api/apt-real/skel.tsx
@@ -1,8 +1,15 @@
 export default function Skel() {
   return (
-    <div className="w-80 bg-white p-4">
-      <div className="animate-pulse flex flex-col">
-        <div className="h-48 mt-2 mb-2 bg-slate-200 rounded"></div>
+    <div>
+      <div className="m-3 p-3">
+        <div className="animate-pulse">
+          <div className="h-6 mt-2 mb-2 bg-slate-200 rounded"></div>
+        </div>
+      </div>
+      <div className="w-full lg:w-80 bg-white p-4">
+        <div className="animate-pulse flex flex-col">
+          <div className="h-36 mt-2 mb-2 bg-slate-200 rounded"></div>
+        </div>
       </div>
     </div>
   )

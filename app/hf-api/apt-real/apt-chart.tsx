90cc739222dbb87076130c21338b907c3de56482
dhkim9549 <dhkim95@hotmail.com>
2024-09-14 07:22:52 +0000

    remove load

--- app/hf-api/apt-real/apt-chart.tsx
+++ app/hf-api/apt-real/apt-chart.tsx
@@ -6,44 +6,16 @@ import "billboard.js/dist/billboard.css";  // default css
 import IconButton from '@mui/material/IconButton';
 import DeleteIcon from '@mui/icons-material/Delete';
 
-export default function AptChart({aptList, clearAptList}) {
+export default function AptChart({setChartRef, clearAptList}) {
 
   let chartRef = useRef();
 
   useEffect(() => {
 
-    if(chartRef.current) {
-      chartRef.current.unload();
-    }
-
-    let chartData = {};
-    chartData.xs = {};
-    chartData.columns = [];
-    let xTickValues = [];
-    let maxY = 0;
-
-    aptList.forEach((apt) => {
-      chartData.xs[apt.aptNm + apt.area] = apt.aptNm + apt.area + '_x';
-      let xArr = [apt.aptNm + apt.area + '_x'];
-      let yArr = [apt.aptNm + apt.area];
-      apt.trd.forEach((trd) => {
-        xArr.push(trd.ctrtDy.substring(0, 4) + '-' + trd.ctrtDy.substring(4, 6) + '-' + trd.ctrtDy.substring(6, 8));
-	yArr.push(trd.prc);
-	if(maxY < trd.prc) {
-          maxY = 1.02 * trd.prc;
-	}
-	if(!xTickValues.includes(trd.ctrtDy.substring(0, 4) + '-01-01')) {
-          xTickValues.push(trd.ctrtDy.substring(0, 4) + '-01-01');
-	}
-      });
-      chartData.columns.push(xArr);
-      chartData.columns.push(yArr);
-    });
-
     var chart2 = bb.generate({
       data: {
-        xs: chartData.xs,
-        columns: chartData.columns,
+        xs: [],
+        columns: [],
         type: scatter(), // for ESM specify as: line()
       },
       axis: {
@@ -51,7 +23,6 @@ export default function AptChart({aptList, clearAptList}) {
           type: "timeseries",
           tick: {
             format: "%Y-%m-%d",
-            values: xTickValues
           },
         },
         y: {
@@ -59,7 +30,6 @@ export default function AptChart({aptList, clearAptList}) {
             text: "만원",
             position: ""
           },
-	  max: maxY,
 	  min: 0,
 	  padding: 0
         }
@@ -83,16 +53,18 @@ export default function AptChart({aptList, clearAptList}) {
     });
 
     chartRef.current = chart2;
+    setChartRef(chart2);
 
-  }, [aptList]);
+  }, []);
 
   return (
     <div className="my-20 ml-5 flex flex-col">
       <div id="chart2"></div>
       <div className="px-10">
         <IconButton aria-label="delete" size="large"
-          disabled={aptList.length > 0 ? false : true}
+          disabled={chartRef.current?.data.length > 0 ? false : true}
           onClick={() => {
+            chartRef.current.unload();
             clearAptList();
 	  }}
 	>

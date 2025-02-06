9392f8c9290895c9f9c6541ac404eb6de610a370
dhkim9549 <dhkim95@hotmail.com>
2025-02-06 00:58:18 +0000

    tag line change

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
     <html lang="ko" className={notoSansKR.className}>
       <body className="">
         <div className="fixed top-0 z-50 w-full bg-slate-800 text-slate-100 text-center p-1">
-          USE IT OR LOSE IT 
+          FAIL FAST
         </div>
 	<div className="">
           {children}

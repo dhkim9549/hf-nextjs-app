cde4be1123f6c6cc92e8a76e2f3677f926439515
dhkim9549 <dhkim95@hotmail.com>
2025-02-07 05:28:09 +0000

    add THREE

--- app/page.tsx
+++ app/page.tsx
@@ -41,6 +41,11 @@ export default function Home() {
           cont="한국주택금융공사 보증서 담보 시중은행 대출의 고객 특성별 금리"
           href="hf-api/rent-loan-multi-info"
         />
+        <Item
+          title="THREE"
+          cont="three.js test"
+          href="hf-api/three-test"
+        />
       </div>
       <Footer/>
     </div>
